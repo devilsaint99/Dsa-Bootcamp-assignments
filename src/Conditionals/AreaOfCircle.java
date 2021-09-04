64fb2e6869a4561ba6c7843aff468eac8d501847
Devilsaint <anshul.suresh99@gmail.com>
2021-09-04 19:27:47 +0530

    added src file

---
+++ src/Conditionals/AreaOfCircle.java
@@ -0,0 +1,17 @@
+package Conditionals;
+
+import java.util.Scanner;
+
+public class AreaOfCircle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter side of square :");
+		int side=sc.nextInt();
+		int area;
+		area=side*side;
+		System.out.println(area);
+	}
+
+}
---
+++ src/Conditionals/Fibonacci.java
@@ -0,0 +1,22 @@
+package Conditionals;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int a1=0,a2=1,a3;
+		System.out.print(a1+" "+a2+" ");
+		for(int i=2;i<n;i++) {
+			a3=a1+a2;
+			
+			a1=a2;
+			a2=a3;
+			System.out.print(a3+" ");
+		}
+	}
+
+}
