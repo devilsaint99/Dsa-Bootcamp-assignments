2310f1b617184ddfe667890d0889f6aa6b201f5f
Devilsaint <anshul.suresh99@gmail.com>
2021-09-07 01:45:16 +0530

    Create Main.java

---
+++ src/Conditionals/Main.java
@@ -0,0 +1,33 @@
+//Kunal is allowed to go out with his friends only on the even days of a given month.
+//Write a program to check if he can go out in the month of August.
+
+package Conditionals;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		while(true) {
+			int inp=sc.nextInt();
+		
+			if(inp>31) {
+				System.out.println("Invalid date, Enter correct date");
+			}
+			else {
+				if(inp%2==0) {
+					System.out.println("Kunal is allowed to go out today");
+				}
+				else {
+					System.out.println("Kunal is not allowed to go out today");
+				}
+				break;
+			}
+		
+		}
+
+	}
+
+}
