bbbd1d8319813a533d27f0b19a797b1cd999c758
Devilsaint <anshul.suresh99@gmail.com>
2021-09-11 21:46:18 +0530

    Create Prime.java
    isPrime Method implementatio

---
+++ src/methods/Prime.java
@@ -0,0 +1,38 @@
+///isPrime Method implementation
+
+package methods;
+
+import java.util.Scanner;
+
+public class Prime {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		System.out.print(isPrime(n));
+	}
+	
+	static boolean isPrime(int num) {
+		boolean check=true;
+		if(num<=1) {
+			check=false;
+		}
+		else if(num==2 || num==3) {
+			check=true;
+		}
+		for(int i=2;i*i<=num;i++) {
+			if(num%i==0) {
+				check= false;
+				break;
+			}
+			
+		}
+		return check;
+		
+	}
+		
+		
+	
+	
+
+}
